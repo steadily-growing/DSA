4f948b4a3c93af01710773bda2d38918076ba155
abena07 <abenabennett@gmail.com>
2023-06-29 16:23:16 +0000

    diameter of a binary tree

---
+++ leetcode/binarytrees/diameterOfBT.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        maxD = [0]
+
+        def dfs(root):
+            if not root:
+                return -1
+            left = dfs(root.left)
+            right = dfs(root.right)
+            maxD[0] = max(maxD[0], 2 + left + right)
+
+            return 1 + max(left, right)
+
+        dfs(root)
+        return maxD[0]
