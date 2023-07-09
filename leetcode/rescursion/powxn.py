1abb4bfdce0999fea927fa7c1c5b4afef746bf38
abena07 <abenabennett@gmail.com>
2023-07-09 04:45:31 +0000

    pow x n

---
+++ leetcode/rescursion/powxn.py
@@ -0,0 +1,15 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+
+        def helper(x,n):
+            if n == 0: return 1
+            if x == 0: return 0
+
+            res = helper(x, n//2)
+            res = res * res
+            odd_res = x * res
+            return odd_res if n % 2 else res
+
+        res = helper(x, float(abs(n)))
+        return res if n >= 0 else 1 / res
+        
