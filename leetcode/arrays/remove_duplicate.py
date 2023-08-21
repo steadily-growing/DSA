4d407c0356c018fa30d9c58f3d80dc98f7f9db00
abena07 <abenabennett@gmail.com>
2023-08-21 20:21:35 +0000

    revised arrays

--- leetcode/arrays/remove_duplicate.py
+++ leetcode/arrays/remove_duplicate.py
@@ -7,3 +7,33 @@ class Solution:
             else:
                 i += 1
         return len(nums)
+    
+## second try i used two pointers
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        """"
+        l  r
+        [1,1,2]
+
+         l        r  
+        [1,2]
+
+        l  r
+        [0,0,1,1,1,2,2,3,3,4]
+           l r
+        [0,1,1,1,2,2,3,3,4]
+
+
+        """
+        l = 0
+        r = l+1
+
+        while r < len(nums):
+            if nums[l] == nums[r]:
+               del(nums[r])
+            else:
+                l+=1
+                r = l+1
+               
+        return len(nums)
+       
--- leetcode/arrays/serach_insert_position.py
+++ leetcode/arrays/serach_insert_position.py
@@ -9,4 +9,22 @@ class Solution:
                 i +=1
         return i 
             
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        """
+
+             l
+        [1,3,5,6]
+           l
+        [1,3,5,6]
+        """
+        i = 0
+
+        while i < len(nums):
+            if nums[i] >= target:
+                return i
+            else:
+                i+=1
+        return i
+        
 
