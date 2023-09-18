2a214eb9057b3f8fb2b48e2e53d85725d864efe7
abena07 <abenabennett@gmail.com>
2023-09-18 12:14:04 +0000

    valid parenthesis

--- leetcode/stacksQueues/valid_parenthesis.py
+++ leetcode/stacksQueues/valid_parenthesis.py
@@ -21,3 +21,19 @@ class Solution:
             return False
 
 
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        hashmap = { ")":"(", "]":"[", "}":"{"}
+
+        for c in s:
+            if c in hashmap:
+                if stack and stack[-1] == hashmap[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+        return True if not stack else False
+
+        
