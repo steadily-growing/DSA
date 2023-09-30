7adda8690c5b7c6814f4fbf6cc62529bb36b7423
abena07 <abenabennett@gmail.com>
2023-09-30 01:07:42 +0000

    getInsertRandom question

---
+++ leetcode/OOP/getInsertRandom.py
@@ -0,0 +1,31 @@
+class RandomizedSet:
+
+    def __init__(self):
+        self.map = defaultdict(int)
+        self.lst = []        
+        self.len = 0
+
+    def insert(self, val: int) -> bool:
+        if val in self.map: return False
+
+        self.lst.append(val)
+        self.map[val] = self.len
+        self.len +=1
+
+        return True
+
+
+    def remove(self, val: int) -> bool:
+        if val not in self.map: return False
+        val_ind = self.map[val]
+        last_element = self.lst[-1]
+
+        self.lst[val_ind], self.map[last_element] = last_element, val_ind
+
+        self.lst.pop()
+        del self.map[val]
+        self.len -=1
+        return True
+
+    def getRandom(self) -> int:
+        return random.choice(self.lst)
