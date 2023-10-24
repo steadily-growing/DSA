5b41f2fcd291292a57625eafb2d933f3ecf64a73
abena07 <abenabennett@gmail.com>
2023-10-24 16:38:47 +0000

    flatten a multilevellinkedlist

---
+++ leetcode/bloomberg/flattenAMultilevelLinkedList.py
@@ -0,0 +1,34 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, prev, next, child):
+        self.val = val
+        self.prev = prev
+        self.next = next
+        self.child = child
+"""
+
+class Solution:
+    def flatten(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        stack = []
+
+        start = head
+
+        while head:
+            if head.child:
+                if head.next:
+                    stack.append(head.next)
+                head.next = head.child
+                head.next.prev = head
+                head.child = None
+
+            if head.next is None and stack:
+                head.next = stack.pop()
+                head.next.prev = head
+
+            head = head.next
+        return start
+  
+
+                
+        
