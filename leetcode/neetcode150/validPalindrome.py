88f4faf86970a45bb8fdda0a34daa198b21ab44b
abena07 <abenabennett@gmail.com>
2024-05-29 23:07:20 +0000

    valid palindrome w two pointers :sparkles:

--- leetcode/neetcode150/validPalindrome.py
+++ leetcode/neetcode150/validPalindrome.py
@@ -7,3 +7,31 @@ class Solution:
                 newStr += c.lower()
         
         return newStr == newStr[::-1]
+
+"""
+
+the above algorithm works perfectly but can be optimized
+
+t(c) = O(n)
+s(c) = O(n)
+
+"""
+
+## optimized solution we use two pointers
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l = 0
+        r = len(s) - 1
+
+        while l < r and  not s[l].isalnum():
+            l += 1
+        
+        while r > l and not s[r].isalnum():
+            r -= 1
+
+        if s[l].lower()  != s[r].lower():
+            return False
+
+    return True
